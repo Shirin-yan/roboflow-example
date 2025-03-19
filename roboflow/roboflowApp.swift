1e87cf3529e9c190afc5d0070d4595ef3f76736a
Shirin <shirin@jackpass.io>
2025-03-19 11:38:20 +0500

    Initial Commit

---
+++ roboflow/roboflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  roboflowApp.swift
+//  roboflow
+//
+//  Created by  Shirin-Yan on 19.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct roboflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
